d333e32d108abae06930360df960f445d19f79a5
Paola Solari <paola.solari@ufl.edu>
2020-09-27 06:35:52 -0400

    pss

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const config = require('./config/config');
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  process.env.MONGODB_URI || config.MongoURI,
   {
-    useMongoClient: true
+    useMongoClient: true, useNewUrlParser: true, useUnifiedTopology: true
   }
 );
+
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //alows us to deal with form data and json data
+
+//Express session middleware
+app.use(
+    session({
+      name: "sid",
+      resave: false,
+      saveUninitialized: false,
+      secret: "secret",
+      store: new MongoStore({ mongooseConnection: mongooseSetup.connection }),
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 1 // 1 day
+      }
+    })
+  );
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Globals
+app.use((req, res, next) => {
+    if (req.session) {
+      res.locals.session = req.session;
+    }
+    next();
+  });
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
